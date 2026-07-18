7f3942ceba7eb88b45e6d5f3db47f6781a3536b0
oneureka <65535758+oneureka@users.noreply.github.com>
2026-07-18 18:07:07 +0800

    Fix dev script path and DB pool env assertion

--- express/src/db.ts
+++ express/src/db.ts
@@ -3,7 +3,7 @@ import * as schema from './schema'
 import mysql from 'mysql2/promise'
 import 'dotenv/config'
 
-const pool = mysql.createPool(process.env.TEAHAUS_DATABASE_URL)
+const pool = mysql.createPool(process.env.TEAHAUS_DATABASE_URL!)
 
 export default drizzle(pool, {
   mode: 'default',

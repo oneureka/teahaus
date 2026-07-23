f02cb5301e610b7672b2816e088620e77f1786fe
oneureka <65535758+oneureka@users.noreply.github.com>
2026-07-23 18:38:38 +0800

    chore: update project config

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,11 @@
-// https://docs.taro.zone/docs/next/babel-config
 module.exports = {
   presets: [
     [
       'taro',
       {
         framework: 'vue3',
-        ts: true,
         compiler: 'vite',
+        ts: true,
         useBuiltIns: process.env.TARO_ENV === 'h5' ? 'usage' : false
       }
     ]
